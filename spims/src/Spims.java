a59d158c3601b8f9fa694f0c459dbb0dd104115a
Tim Briggs <timothydbriggs@gmail.com>
2013-02-10 13:56:22 -0500

    Added base input and output to main function.

--- spims/src/Spims.java
+++ spims/src/Spims.java
@@ -1,3 +1,4 @@
+import java.io.*;
 
 public class Spims {
 
@@ -6,7 +7,35 @@ public class Spims {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String inputFile = "";
+        boolean reduce = false;
+        //Single argument given                                                                                                                                                                                                    
+        if (args.length == 1) {
+                inputFile = args[0];
+        }
+        //Multiple arguments given, check for '-r' flag                                                                                                                                                                            
+        else if (args.length >= 2) {
+                inputFile = args[1];
+        }
+        else {
+                System.err.println("Invalid command, one or more arguments required.");
+                return;
+        }
+        try{
+                //Open FileReader to view file contents                                                                                                                                                                            
+                FileReader fr = new FileReader(inputFile);
+                String inputString = new BufferedReader(fr).readLine();
+                //Call parse function on the input and pass in the reduce flag bit                                                                                                                                                 
+                //String prefix = Prefixer.parseInfix(inputString, reduce);
+                //Print the input string before and after to the console                                                                                                                                                           
+                System.out.println(inputString + " -> " + prefix);
+                //Close FileReader                                                                                                                                                                                                 
+                fr.close();
+        }
+        catch (Exception e){
+                System.err.println("Error: " + e.getMessage());
+        }
+}
 	}
 
 }
