35bd32a53285efdead6c9d3b3b9ded70e9e33f57
Corey Hanson <corey@practicallygreen.com>
2013-02-26 17:37:39 -0500

    ignore hidden files

--- spims/src/ImageFilter.java
+++ spims/src/ImageFilter.java
@@ -9,11 +9,12 @@ public class ImageFilter implements FileFilter{
   }
 
   public boolean accept(File f){
+    if(f.getName().toLowerCase().startsWith(".")){ return false; }
     for(int i = 0; i < imageTypes.length; i++){
       if(f.getName().toLowerCase().endsWith(imageTypes[i])){ return true; }
     }
     System.err.println("Found invalid input for file " + f.getName());
-    System.exit(0);
+    System.exit(1);
     return false;
   }
 }
