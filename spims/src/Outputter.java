4c09aaa1c2ef417ffb9d24abd2b12cefcd00525b
Reed Lockwood <reed.w.lockwood@gmail.com>
2013-02-28 17:22:37 -0500

    Added filter funciton

--- spims/src/Outputter.java
+++ spims/src/Outputter.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 class MatchData{
-    private String pattern;
-    private String source;
-    private int width;
-    private int height;
-    private int x;
-    private int y;
+    public String pattern;
+    public String source;
+    public int width;
+    public int height;
+    public int x;
+    public int y;
+    public int size;
+    public int x2;
+    public int y2;
 
     public MatchData(String _pat, String _src, int _w, int _h, int _x, int _y){
         pattern = _pat;
@@ -16,6 +19,10 @@ class MatchData{
         height = _h;
         x = _x;
         y = _y;
+        //FENCEPOSTING?
+        size = (width + 1) * (height + 1);
+        x2 = x + width;
+        y2 = y + height;
     }
 
     public String toString(){
@@ -40,8 +47,35 @@ public class Outputter{
         return myMatches.size();
     }
 
+    //THIS FUNCTION ASSUMES BOTH FOUND PATTERNS ARE THE SAME SIZE.  UPDATE LATER
+    //FENCEPOSTING on width and height?
+    public void filter(){
+        //compare each match to every match that comes after it
+       for(Iterator<MatchData> iter = myMatches.iterator(); iter.hasNext();){
+           MatchData first = iter.next();
+           for(Iterator<MatchData> secondIter = (Iterator<MatchData>) iter.clone(); secondIter.hasNext();){
+               MatchData second = secondIter.next();
+               if(first.pattern.equals(second.pattern) && first.source.equals(second.source)){
+                   //find the (possibly non-existant) rectangle where the two matches overlap
+                   int width = min(first.x2, second.x2) - max(first.x, second.x) + 1;
+                   int height = min(first.y2, second.y2) - max(first.y, second.y) + 1;
+                   if(width > 0 && height > 0){
+                       float crossover = (float)(width * height);
+                       //is it greater than half the size of the first match?
+                       if(crossover > .5 * ((float) first.size)){
+                           //get rid of the first match
+                           iter.remove();
+                           break;
+                       }
+                   }
+               }
+           }
+       }
+    }
+
     public void output(){
         //boolean firstline = true;
+        filter();
         int i = 0;
         while(i < myMatches.size()){
             System.out.println(myMatches.get(i).toString());
