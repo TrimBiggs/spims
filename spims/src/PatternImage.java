41a30f63475ea2a40ed8545492f9f90d84bbe03f
Tim Briggs <timothydbriggs@gmail.com>
2013-04-12 12:39:06 -0400

    Created class to store pattern pixels array with the offset of tolerance

---
+++ spims/src/PatternImage.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+/**
+*    Class Pixel holds an rgb value for a pixel in an image.
+* It has comparison functions to use with other pixels.
+*
+*   @author Corey Hanson, Tim Briggs, Reed Lockwood, Wen Cao
+*	@version 0.4
+*	@since 1/30/2012/
+*
+*/
+public class PatternImage {
+
+	/**
+	*	Constructor for class PatternImage.
+	*
+	*	@param _image The array of Pixel RGB values that make up the image
+	* 	@return PatternImage. Return the newly created PatternImage object
+	*/
+	public PatternImage(Array[][] _pixels){
+		pixels = _pixels;
+		offset = 0;
+	}
+}
